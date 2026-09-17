e5d98b6519d6f9f0a24963f292dc04bec5ef070b
Joseph-Jel <joseph.jello@student.uts.edu.au>
2026-09-18 00:36:23 +1000

    Implemented the function BuildFullMap()

--- 14480709_Asses3/Assets/Scripts/LevelGenerator.cs
+++ 14480709_Asses3/Assets/Scripts/LevelGenerator.cs
@@ -104,6 +104,41 @@ public class LevelGenerator : MonoBehaviour
         AdjustCamer();
     }
 
+    private void BuildFullMap()
+    {
+        int rows = levelMap.GetLength(0);
+        int columns = levelMap.GetLength(1);
+
+        int fullRows = (rows * 2) - 1;
+        int fullColumns = columns * 2;
+
+        fullMap = new int[fullRows, columns];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+
+                int value = levelMap[row, column];
+
+                fullMap[row, column] = value;
+
+                int mirroredColumn = fullColumns - 1 - column;
+
+                fullMap[row, mirroredColumn] = value;
+
+                if (row < rows - 1)
+                {
+                    int mirroredRow = fullRows - 1 - row;
+
+                    fullMap[mirroredRow, column] = value;
+
+                    fullMap[mirroredRow, mirroredColumn] = value;
+                }
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
